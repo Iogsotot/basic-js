50dc4b67db6d82f76e82182f10c011017541298c
Iogsotot <ajustusa@gmail.com>
2020-09-30 00:29:46 +0300

    feat: carbon dating resolution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sampleActivityNumber = parseInt(sampleActivity, 10);
+  if (isNaN(sampleActivityNumber) || !sampleActivityNumber || sampleActivityNumber == 'undefined' || sampleActivity <= 0 ||  sampleActivity > 15 || typeof(sampleActivity) !== 'string' ) { 
+    return false; 
+  }
+  const ln = 0.693;
+  const t = Math.ceil(Math.log(sampleActivity / MODERN_ACTIVITY ) / ln * HALF_LIFE_PERIOD) * -1;
+  return t;
 };
